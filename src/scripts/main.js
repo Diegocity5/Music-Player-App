6909859b3a7f316a2cd285bd2678c8c3a3f1c3e5
Diegocity5 <icotecc@gmail.com>
2024-10-08 18:45:38 -0400

    26: Invocando la funcion de renderSongs y mandando como argumento el valor de la propiedad songs del objeto userData que es un array y uso de optional chaining para prevenir errores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -97,7 +97,7 @@ const renderSongs = (array)=>{
             <span class="playlist-song-title">${song.title}</span>
             <span class="playlist-song-artist">${song.artist}</span>
             <span class="playlist-song-duration">${song.duration}</span>
-        </button
+        </button>
         <button class="playlist-song-delete" aria-label="Delete ${song.title}">
             <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
         </button>
@@ -106,3 +106,11 @@ const renderSongs = (array)=>{
 
     playlistSongs.innerHTML = songsHTML;//Insertando li songs en el contenedor de la playlist.
 };
+
+/*El optional chaining (?.) es una característica de JavaScript
+que permite acceder a propiedades de objetos anidados de manera segura.
+Si una propiedad no existe en algún nivel de la cadena, en lugar de lanzar un error,
+devuelve undefined.
+Esto es útil para evitar errores al acceder a propiedades que pueden no estar definidas,
+haciendo que el código sea más limpio y fácil de mantener*/
+renderSongs(userData?.songs);
