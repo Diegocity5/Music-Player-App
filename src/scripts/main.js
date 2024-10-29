c7256c8013cb3ae51c5960e44c1b732358b09c35
Diegocity5 <icotecc@gmail.com>
2024-10-29 00:58:57 -0400

    55: Accediendo a la song siguiente del que estabmos sumamos uno al indice altual para que busque el consecutivo en el array de songs y despues reproducimos esa cancion siguiente

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -165,6 +165,8 @@ const playNextSong = ()=>{
         playSong(userData?.songs[0].id);
     }else {
         const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];//accediendo a la cancion consecutiva
+        playSong(nextSong.id);
     }
 }
 
