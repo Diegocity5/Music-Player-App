b6a5ab854bd91edd8cd7d20a2bc471eba5b8f422
Diegocity5 <icotecc@gmail.com>
2024-11-14 21:30:28 -0500

    79: Renderizando nuevamente las canciones, pausando la canción que se está reproduciendo actualmente, configurarando la pantalla del reproductor y configurarando nuevamente el texto accesible del botón de reproducción.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -231,6 +231,10 @@ const shuffle = ()=>{
     userData?.songs.sort(()=> Math.random() - 0.5);
     userData.currentSong = null;
     userData.songCurrentTime = 0;
+    renderSongs(userData?.songs);//renderizar canciones barajadas.
+    pauseSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
 }
 
 
